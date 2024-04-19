a24e99da7857f0f4d471c54bf557888a5b41c549
Sabbir <ahmedsabbir401@gmail.com>
2024-04-19 20:22:40 +0600

    profile api calling finish

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
-const CLIENT_ID = 'aaea19153f6e1b12cd27';
+const CLIENT_ID = "aaea19153f6e1b12cd27";
 
-const CLIENT_SECRET = '96743b8d5e5057f7de84386c4c898cc8c5e56abb'
+const CLIENT_SECRET = "96743b8d5e5057f7de84386c4c898cc8c5e56abb";
 
 async function getUser(name) {
-    const res = await fetch(`https://api.github.com/users/${name}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`);
+  const res = await fetch(
+    `https://api.github.com/users/${name}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+  );
 
-    const profile = await res.json();
+  const profile = await res.json();
 
-    return profile;
+  return profile;
 }
 
-document.querySelector('#search').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const username = document.querySelector('#findByUsername').value;
+document.querySelector("#search").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const username = document.querySelector("#findByUsername").value;
 
-    console.log(username);
+  console.log(username);
 
-    const profile = await getUser(username);
+  const profile = await getUser(username);
 
-    console.log(profile);
+  showProfile(profile);
+  console.log(profile);
+});
 
-})
+function showProfile(profile) {
+  document.querySelector(".profile").innerHTML = `
+    <img
+        src="${profile.avatar_url}"
+        alt="${profile.name}"
+        />
+        <p class="name">${profile.name}</p>
+        <p class="username login">${profile.login}</p>
+        <p class="bio">
+        ${profile.bio}
+        </p>
+
+        <div class="followers-stars">
+        <p>
+            <ion-icon name="people-outline"></ion-icon>
+            <span class="followers"> ${profile.followers} </span> followers
+        </p>
+        <span class="dot">·</span>
+        <p><span class="following"> ${profile.following} </span> following</p>
+        </div>
+
+        <p class="company">
+        <ion-icon name="business-outline"></ion-icon>
+        ${profile.company}
+        </p>
+        <p class="location">
+        <ion-icon name="location-outline"></ion-icon>${profile.location}
+        </p>
+    `;
+}
